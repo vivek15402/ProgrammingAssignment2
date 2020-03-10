7222be6ce9eb1b3eaa29ff2ce6a6080951181092
vivek15402 <44522137+vivek15402@users.noreply.github.com>
2020-03-10 11:55:32 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,19 +2,20 @@
 ## This function creates a special "matrix" object that can cache its inverse.
 ## Initialize inverse with NULL then initialize x to get_matrix function, initialize setInverse and so on...
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix()) {             ## define the argument with default mode of matrix
         inverse <- NULL
-        set_matrix <- function(y){
-                x <<- y
-                inverse <<- NULL
+        set_matrix <- function(y){                      ## define the set function
+                x <<- y                                 ## This represents value of matrix in parent environment
+                inverse <<- NULL                        ## to reset inverse to null if there is a new matrix
         }
-        get_matrix <- function() x
-        setInverse <- function(solveMatrix) inverse <<- solveMatrix
-        getInverse <- function() inverse
-        list(set_matrix = set_matrix, get_matrix = get_matrix, setInverse = setInverse, getInverse = getInverse)
+        get_matrix <- function() x                      ## returns the value of matrix argument
+        setInverse <- function(solveMatrix) inverse <<- solveMatrix     ## assign matrix value to parent environment
+        getInverse <- function() inverse                                ## it gets the value of inverse where called
+        list(set_matrix = set_matrix, get_matrix = get_matrix, setInverse = setInverse, getInverse = getInverse)  ## used to refer tp the function woth $ operator
 }
 
 ## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+## if the inverse oid already calculated then inverse matrix will be retreived from cache
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         inverse <- x$getInverse()
